77ecad02d18a3b026793f3c09e44a41361ae6ae9
Dan Lofaro <dan@danlofaro.com>
2019-02-27 20:38:31 -0500

    working with reducing volume

--- drum-poem/drum-poem-v1/drum-poem-v1.ino
+++ drum-poem/drum-poem-v1/drum-poem-v1.ino
@@ -26,6 +26,17 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=99,88
 int current_to_play = 1;
 int flag_play = 0;
 
+/* set volume timer for intergration */
+IntervalTimer volumeTimer;
+IntervalTimer playTimer;
+
+/* volume intergrater */
+double volume_intergrater = 0.0;
+double volume_intergrater_k_input = 0.01;
+double volume_intergrater_k_reduce = 0.001;
+#define volume_intergrater_max 1.0
+#define volume_min 0.3
+
 void setup() {
   // put your setup code here, to run once:
   AudioMemory(8);
@@ -57,13 +68,27 @@ void setup() {
 
   //attachInterrupt(digitalPinToInterrupt(8), play1i, RISING );
   //attachInterrupt(digitalPinToInterrupt(5), play2i, RISING );
-  attachInterrupt(digitalPinToInterrupt(8), play_next, RISING );
+  //attachInterrupt(digitalPinToInterrupt(8), play_next, RISING );
   attachInterrupt(digitalPinToInterrupt(5), play_next, RISING );
-  
-}
 
+  /* Start volume timer */
+  volumeTimer.begin(doVolumeTimer, 10000);
+  //playTimer.begin(doPlayTimer,10000);
+}
+int tmp = 0;
 void play_next(){
   flag_play = 1;
+  noInterrupts();
+  double tmp_vol = volume_intergrater;
+  tmp_vol = tmp_vol + volume_intergrater_k_input;
+  if(tmp_vol > volume_intergrater_max) tmp_vol = volume_intergrater_max;
+  volume_intergrater = tmp_vol;
+  interrupts();
+  tmp_vol = tmp_vol / volume_intergrater_max;
+  if(tmp_vol < volume_min) tmp_vol = volume_min;
+  sgtl5000_1.volume(tmp_vol);
+  
+  Serial.println(tmp_vol);
   return;
 }
 
@@ -71,7 +96,8 @@ void play_next(){
 void block()
 {
   digitalWrite(13, HIGH);
-  while(sd.isPlaying() != 0);
+  //while(sd.isPlaying() != 0);
+  while(sd.isPlaying() == 0);
   //for(int i = 0; i < NUM_TO_PLAY; i++) toplay[i] = 0;
   digitalWrite(13, LOW);
   return;
@@ -145,20 +171,44 @@ void play2i() {
 }
 */
 
-
-void loop() {
+void doVolumeTimer() {
+  double tmp_vol = volume_intergrater;
+  tmp_vol = tmp_vol - volume_intergrater_k_reduce;
+  if(tmp_vol < volume_min) tmp_vol = volume_min;
+  volume_intergrater = tmp_vol;
+  tmp_vol = tmp_vol / volume_intergrater_max;
+  if(tmp_vol < volume_min) tmp_vol = volume_min;
+  sgtl5000_1.volume(tmp_vol);
+  Serial.println(tmp_vol);
+  //yield();
+  return;
+}
+void doPlayTimer() {
   // put your main code here, to run repeatedly:
  if(1 == flag_play){
- 
-  if(1 == current_to_play)      {play1(); block(); current_to_play++;}
-  else if(2 == current_to_play) {play2(); block(); current_to_play++;}
-  else if(3 == current_to_play) {play3(); block(); current_to_play++;}
-  else if(4 == current_to_play) {play4(); block(); current_to_play++;}
-  else if(5 == current_to_play) {play5(); block(); current_to_play++;}
-  else if(6 == current_to_play) {play6(); block(); current_to_play++;}
-  else if(7 == current_to_play) {play7(); block(); current_to_play = 1;}
   flag_play = 0;
+ 
+  if(1 == current_to_play)      {play1();}
+  else if(2 == current_to_play) {play2();}
+  else if(3 == current_to_play) {play3();}
+  else if(4 == current_to_play) {play4();}
+  else if(5 == current_to_play) {play5();}
+  else if(6 == current_to_play) {play6();}
+  else if(7 == current_to_play) {play7();}
+  block();
+
+  current_to_play++;
+  if(current_to_play > NUM_TO_PLAY) current_to_play = 1;
+  
  }
+ //yield();
+ return;
+}
+
+void loop() {
+  //noInterrupts();
+  //interrupts();
+  doPlayTimer();
   delay(1);
 
 }
